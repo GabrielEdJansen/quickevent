1ae68a009542b5fa5f9cf88a22a99a0996c9da98
GabrielEdJansen <gabrieljans18@gmail.com>
2023-12-22 10:32:10 -0300

    Config servidor

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ import pandas as pd
 import pymysql
 from datetime import datetime
 import re
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "gg123"
+CORS(app, resources={r"/decode-token/*": {"origins": "https://www.quickevent.com.br/"}})
 
 @app.route("/")
 def home():
