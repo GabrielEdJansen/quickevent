9e7dce8019ee04771c638d732140e88d1cad97cd
GabrielEdJansen <gabrieljans18@gmail.com>
2023-12-22 14:25:39 -0300

    Config servidor

--- main.py
+++ main.py
@@ -27,15 +27,21 @@ def cadastrar():
 
 @app.route('/decode-token/<token>', methods=['GET'])
 def decode_token(token):
-        # Aqui, você decodifica o token usando a chave secreta utilizada para criptografá-lo
-        return jsonify({"decoded_token": token})
-        #decoded_token = jwt.decode(token, '9b0285c31bfd8b040e03157b19c4e960bdc10c6f', algorithms=['RS256'])
+    try:
+        token = request.json.get('id_token')  # Supondo que o token seja enviado no corpo da requisição com a chave 'id_token'
+
+        if not token:
+            return jsonify({"error": "Token não fornecido"}), 400
+
+        # Faça a verificação do token sem chave pública (HS256)
+        decoded_token = jwt.decode(token, options={"verify_signature": False})
+
         # Faça o que precisar com os dados decodificados (decoded_token)
-        #return jsonify({"decoded_token": decoded_token})
-        #except jwt.ExpiredSignatureError:
-        #return jsonify({"error": "Token expirado"})
-        #except jwt.InvalidTokenError:
-        #return jsonify({"error": "Token inválido"})
+        return jsonify({"decoded_token": decoded_token})
+    except jwt.ExpiredSignatureError:
+        return jsonify({"error": "Token expirado"}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({"error": "Token inválido"}), 401
 
 @app.route("/cadastro", methods=['POST'])
 def cadastro():
