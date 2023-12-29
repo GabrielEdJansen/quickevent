da06bf5858ecd8d5e72ab269174d1a033a0f703c
GabrielEdJansen <gabrieljans18@gmail.com>
2023-12-29 09:46:55 -0300

    Config servidor

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def cadastro():
     conexao.close()
 
     #return render_template("html/login.html", nomecadastro=nomecad + " cadastrado!")
-    return redirect(url_for("login"))
+    return redirect(url_for("logininicio"))
 
 
 @app.route("/login", methods=['POST'])
