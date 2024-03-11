7c2c8309b84b59a2f38781d307a804d3fe00ecc3
Gabriel Eduardo Jansen <gabrieljans18@gmail.com>
2024-03-11 17:52:00 -0300

    +

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, jsonify, url_for
+from flask import Flask, render_template, request, flash, redirect, jsonify, url_for, get_flashed_messages
 from banco import configbanco
 import jwt
 import base64
