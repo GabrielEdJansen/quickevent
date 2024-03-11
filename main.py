acd7c1a6af28eabb2f13ef881cff3bdc257f16aa
Gabriel Eduardo Jansen <gabrieljans18@gmail.com>
2024-03-11 01:08:52 -0300

    +

--- main.py
+++ main.py
@@ -95,8 +95,7 @@ def SalvarAlteracoes():
     dataCad = request.form.get('dataCad')
     dataCadFin = request.form.get('dataCadFin')
 
-    #horCad = request.form.get('horCad')
-    horCad = datetime.strptime(request.form.get('horCad'), "%H:%M").time()
+    horCad = request.form.get('horCad')
 
     horCadFin = request.form.get('horCadFin')
     data_atual = datetime.now().date()
