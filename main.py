e6b3625c2937aa2bb6c417e8c80c47c6ea7b9b10
Gabriel Eduardo Jansen <gabrieljans18@gmail.com>
2024-03-13 23:44:04 -0300

    +

--- main.py
+++ main.py
@@ -307,8 +307,8 @@ def processarPresenca():
                 # Verificar se quantidade_maxima e quantidadeConvitesInt são do tipo int
                 if isinstance(quantidade_maxima, int) and isinstance(quantidadeConvitesInt, int):
                     # Se forem inteiros, comparar os valores
-                    if quantidadeConvitesInt> quantidade_maxima:
-                        flash("A quantidade máxima de ingressos por usuário é " + str(quantidadeConvites) + "!")
+                    if quantidadeConvitesInt > quantidade_maxima:
+                        flash("A quantidade máxima de ingressos por usuário é " + str(quantidade_maxima) + "!")
                         return render_template("html/InformacoesEventos.html", eventos=eventos, foto=foto, ingresso=ingresso)
                 else:
                     flash("Erro ao comparar as quantidades. Verifique os valores.")
