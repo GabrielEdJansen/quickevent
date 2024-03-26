5648ea0f36b84c2c6f4c019a0f2f817513a35740
Gabriel Eduardo Jansen <gabrieljans18@gmail.com>
2024-03-26 20:15:17 -0300

    +

--- static/authHandler.js
+++ static/authHandler.js
@@ -1,92 +1,43 @@
  function handleCredentialResponse(response) {
-            const token = response.credential;
+    const token = response.credential;
 
-            return fetch(`/decode-token/${token}`, {
-                method: 'POST',
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Erro na requisição');
-                }
-                return response.json();
-            })
-            .then(decodedData => {
-                var given_name = document.getElementById('nomecad');
-                var family_name = document.getElementById('sobrenomecad');
-                var email = document.getElementById('emailcad');
-                var subId = document.getElementById('subId');
-                var eventoPresenca = document.getElementById('eventoPresenca').value;
+    fetch(`/decode-token/${token}`, {
+        method: 'POST',
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro na requisição');
+        }
+        return response.json();
+    })
+    .then(decodedData => {
+        var emailValue = decodedData.email; // Obtém o valor do email de decodedData
+        var subIdValue = decodedData.sub; // Obtém o valor da senha de decodedData
 
-                given_name.textContent = decodedData.given_name
-                family_name.textContent = decodedData.family_name
-                email.textContent = decodedData.email
-                subId.textContent = decodedData.sub
-
-                given_name.value = decodedData.given_name;
-                family_name.value = decodedData.family_name;
-                email.value = decodedData.email;
-                subId.value = decodedData.sub;
-
-                const form = document.getElementById('meuFormulario');
-
-                // Código para enviar os dados do formulário (exemplo com fetch) ao carregar a página
-                fetch(form.action, {
-                    method: form.method,
-                    body: new FormData(form)
-                })
-                .then(response => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        throw new Error('Erro ao enviar requisição.');
-                    }
-                })
-                .then(data => {
-                    console.log('Requisição enviada com sucesso!', data);
-                    // Faça algo com os dados, se necessário
-                })
-                .catch(error => {
-                    console.error('Erro ao enviar requisição:', error);
-                });
-
-                var emailValue = decodedData.email; // Obtém o valor do email de decodedData
-                var subIdValue = decodedData.sub; // Obtém o valor da senha de decodedData
-
-                // Realiza a requisição POST com os dados do formulário
-                fetch('/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: 'email=' + encodeURIComponent(emailValue) + '&senha=' + encodeURIComponent(subIdValue) + '&eventoPresenca=' + eventoPresenca
-                })
-                .then(response => {
-                    if (response.ok) {
-                        // Realizaaar ações após o envio bem-sucedido, se necessário
-                        if (eventoPresenca > 0){
-                            console.log('Dados enviados com sucesso');
-                            //window.location.href = '/InformacoesEventos?eventoPresenca=' + eventoPresenca;
-                        } else{
-                            console.log('Dados enviados com sucesso');
-                            //window.location.href = '/destaques';
-                        }
-                    } else {
-                        throw new Error('Erro ao enviar dados');
-                    }
-                })
-                .catch(error => {
-                    console.error('Erro:', error);
-                })
-                .finally(() => {
-                });
-
-                return decodedData;
-            })
-            .catch(error => {
-                console.error('Erro:', error);
-                throw error;
-            });
- }
+        // Realiza a requisição POST com os dados do formulário
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: 'email=' + encodeURIComponent(emailValue) + '&senha=' + encodeURIComponent(subIdValue)
+        })
+        .then(response => {
+            if (response.ok) {
+                // Se a resposta estiver OK, submeta o formulário
+                document.getElementById('loginForm').submit();
+            } else {
+                throw new Error('Erro ao enviar dados');
+            }
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+}
 window.onload = function() {
     google.accounts.id.initialize({
         client_id: "1065269498355-b9lre71ptvnlqp5jombpjkg0snsctipe.apps.googleusercontent.com",
